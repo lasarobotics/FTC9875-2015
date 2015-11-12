a87090001446d85d6936c5c41e218aa31196204e
PokeMaster100 <rishirules01@gmail.com>
2015-11-12 17:14:04 -0600

    Update Autonomous.java
    Since we will not be using random movements (anymore) we do not need the line "import java.util.Random;", nor do we need the line, "Random rand = new Random();".

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/Autonomous.java
@@ -2,14 +2,12 @@ package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import java.util.Random;
-
 /**
  * Created by will on 11/7/15.
+ * Cleaned by Rishi on 11/12/15.
  */
 public class Autonomous extends OpMode {
     DcMotor left_back_motor, left_front_motor, right_back_motor, right_front_motor;
-    Random rand = new Random();
     public void init() {
         left_back_motor = hardwareMap.dcMotor.get("lb");
         left_front_motor = hardwareMap.dcMotor.get("lf");
