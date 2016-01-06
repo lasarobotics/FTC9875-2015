59d2d8250532ba45a5c31ef23fdb7e3d770ddd8c
WSlatton <williamslatton11@gmail.com>
2016-01-06 16:30:40 -0600

    formatted

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOp.java
@@ -1,4 +1,4 @@
-package  com.qualcomm.ftcrobotcontroller.opmodes;
+package com.qualcomm.ftcrobotcontroller.opmodes;
 
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
