39046e8df14b00c40201b1275333c9a094d885fc
WSlatton <williamslatton11@gmail.com>
2016-01-12 17:43:54 -0600

    added pulley and winch control

--- FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpv3.java
+++ FtcRobotController/src/main/java/com/qualcomm/ftcrobotcontroller/opmodes/TeleOpv3.java
@@ -4,18 +4,21 @@ import com.lasarobotics.library.util.SmartMotor;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.Servo;
+import com.qualcomm.robotcore.util.RobotLog;
 
 /**
  * Created by will on 1/9/16.
  */
 
 public class TeleOpv3 extends OpMode {
-    SmartMotor left_motor, right_motor;
+    SmartMotor left_motor, right_motor, pulley_motor, winch_motor;
     double speed_scale = 1;
 
     public void init() {
-        left_motor = new SmartMotor(this, "l");
-        right_motor = new SmartMotor(this, "r");
+        left_motor = new SmartMotor(this, "left");
+        right_motor = new SmartMotor(this, "right");
+        pulley_motor = new SmartMotor(this, "pulley");
+        winch_motor = new SmartMotor(this, "winch");
     }
     public void loop() {
         speed_scale = gamepad1.a ? 0.5 : (gamepad1.b ? 0.25 : 1);
@@ -28,6 +31,18 @@ public class TeleOpv3 extends OpMode {
         } else {
             left_motor.setPower(-gamepad1.left_stick_y * speed_scale);
             right_motor.setPower(gamepad1.right_stick_y * speed_scale);
+        }
+
+        if(gamepad1.dpad_up) {
+            pulley_motor.setPower(0.25);
+        } else if(gamepad1.dpad_down) {
+            pulley_motor.setPower(-0.25);
+        }
+
+        if(gamepad1.dpad_left) {
+            winch_motor.setPower(0.25);
+        } else if(gamepad1.dpad_right) {
+            winch_motor.setPower(-0.25);
         }
     }
     public void stop() {
